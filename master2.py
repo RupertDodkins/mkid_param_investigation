3761b9fcc33856e9dc1d6d231605f35521ed2929
rdodkins <rupertdodkins@gmail.com>
2020-04-21 08:29:22 -0700

    Add master params module and figure 1 optics diagram

---
+++ master2.py
@@ -0,0 +1,110 @@
+import numpy as np
+import pickle
+
+from medis.params import params
+import medis.MKIDs as mkids
+
+params['sp'].show_wframe = False
+params['sp'].save_obs = False
+params['sp'].show_cube = False
+params['sp'].num_processes = 1
+params['sp'].save_fields = False
+params['sp'].save_ints = True
+params['sp'].cont_save = True
+params['sp'].grid_size = 512
+params['sp'].closed_loop = False
+
+params['ap'].companion = True
+params['ap'].star_flux = int(1e4)
+# params['ap'].contrast = 10**np.array([-3.5, -4, -4.5, -5] * 2)
+# params['ap'].companion_xy = [[2.5,0], [0,3], [-3.5,0], [0,-4], [4.5,0], [0,5], [-5.5,0],[0,-6]]
+# params['ap'].n_wvl_init = 8
+# params['ap'].n_wvl_final = 16
+params['ap'].contrast = [10**-3.5]
+params['ap'].companion_xy = [[2.5,0]]
+params['ap'].n_wvl_init = 2
+params['ap'].n_wvl_final = 2
+
+params['tp'].prescription = 'general_telescope'
+params['tp'].beam_ratio = 0.25
+# sp.save_locs = np.empty((0, 1))
+params['tp'].entrance_d = 8.
+params['tp'].obscure = True
+params['tp'].use_ao = True
+params['tp'].include_tiptilt = False
+params['tp'].ao_act = 50
+params['tp'].platescale = 10  # mas
+params['tp'].detector = 'ideal'
+params['tp'].use_atmos = True
+params['tp'].use_zern_ab = False
+params['tp'].occulter_type = 'Vortex'
+params['tp'].aber_params = {'CPA': True,
+                         'NCPA': True,
+                         'QuasiStatic': False,  # or Static
+                         'Phase': True,
+                         'Amp': False,
+                         'n_surfs': 4}
+params['tp'].aber_vals = {'a': [5e-18, 1e-19],
+                       'b': [2.0, 0.2],
+                       'c': [3.1, 0.5],
+                       'a_amp': [0.05, 0.01]}
+params['tp'].piston_error = False
+params['ap'].wvl_range = np.array([800, 1500]) /1e9
+params['tp'].rot_rate = 0  # deg/s
+params['tp'].pix_shift = [[0, 0]]
+params['tp'].satelite_speck = True
+params['tp'].legs_frac = 0.03
+params['tp'].f_lens = 200.0 * params['tp'].entrance_d
+params['tp'].use_coronagraph = True
+params['tp'].occult_loc = [0,0]
+params['tp'].cg_type = 'Gaussian'
+params['tp'].cg_size = 3  # physical size or lambda/D size
+params['tp'].cg_size_units = "l/D"  # "m" or "l/D"
+params['tp'].lyot_size = 0.75  # units are in fraction of surface blocked
+params['tp'].fl_cg_lens = params['tp'].f_lens  # m
+
+# for param in [params['sp']]:
+#     pprint(param.__dict__)
+params['mp'].phase_uncertainty = True
+params['mp'].phase_background = False
+params['mp'].QE_var = True
+params['mp'].bad_pix = True
+params['mp'].dark_counts = True
+params['mp'].dark_pix_frac = 0.1
+params['mp'].dark_bright = 20
+params['mp'].hot_pix = None
+params['mp'].hot_bright = 2.5 * 10 ** 3
+params['mp'].R_mean = 8
+params['mp'].R_sig = 2
+params['mp'].R_spec = 1.
+params['mp'].g_mean = 0.3
+params['mp'].g_sig = 0.04
+params['mp'].bg_mean = -10
+params['mp'].bg_sig = 40
+params['mp'].pix_yield = 0.9
+params['mp'].array_size = np.array([150, 150])
+params['mp'].lod = 6
+params['mp'].remove_close = False
+params['mp'].quantize_FCs = True
+params['mp'].wavecal_coeffs = [1.e9 / 6, -250]
+
+def get_form_photons(fields, dp, photons_save, mp, comps=True):
+    ntime, nwave = fields.shape[0], fields.shape[1]
+
+    stackcube = np.zeros((ntime, nwave, dp.array_size[1], dp.array_size[0]))
+    for step in range(len(fields)):
+        print(step)
+        if comps:
+            spectralcube = np.sum(fields[step], axis=1)
+        else:
+            spectralcube = fields[step, :, 0]
+
+        step_packets = mkids.get_packets(spectralcube, step, dp, mp)
+        cube = mkids.make_datacube_from_list(step_packets, (nwave,dp.array_size[0],dp.array_size[1]))
+        stackcube[step] = cube
+
+    with open(photons_save, 'wb') as handle:
+        # dprint((photons_save, stackcube.shape, dp))
+        pickle.dump((stackcube, dp), handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    return stackcube, dp
---
+++ run_optics_fig.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pprint import pprint
+
+from medis.twilight_colormaps import sunlight
+from matplotlib.colors import LogNorm
+import medis.medis_main as mm
+
+from master2 import params
+
+params['sp'].save_to_disk = True
+params['sp'].numframes = 1
+params['ap'].n_wvl_init = 3
+params['ap'].n_wvl_final = 3
+params['sp'].memory_limit = 9.e9 /15
+params['sp'].save_list = np.array(['atmosphere', 'add_aber', 'deformable mirror', 'add_aber', 'pre_coron', 'detector'])
+save_labels = np.array(['Entrance Pupil', 'After CPA', 'After DM', 'After NCPA', 'Before Occult.', 'Detector'])
+
+if __name__ == '__main__':  # required for multiprocessing - make sure globals are set before though
+
+    sim = mm.RunMedis(params=params, name='figure1', product='fields')
+    observation = sim()
+    cpx_sequence = observation['fields']
+
+    # fp_sampling = sampling[-1][:]
+    # print(fp_sampling, cpx_sequence.shape)
+    spectral_train_phase = np.angle(cpx_sequence[0,:-2,:,0])
+    spectral_train_amp = np.abs(cpx_sequence[0,-2:,:,0]**2)
+    spectral_train_grid = np.concatenate((spectral_train_phase,spectral_train_amp), axis=0)
+    crop = spectral_train_grid.shape[2] // 4
+    spectral_train_grid = spectral_train_grid[:,:,crop:-crop,crop:-crop]
+    fig, axes = plt.subplots(6,3, figsize=(4,7))
+
+    for j in range(3):
+        for i in range(2):
+            im1 = axes[i, j].imshow(spectral_train_grid[i, j], cmap=sunlight, vmin=-np.pi, vmax=np.pi)
+        for i in range(2,4):
+            im2 = axes[i, j].imshow(spectral_train_grid[i, j], cmap=sunlight, vmin=-1, vmax=1)
+        for i in range(4,6):
+            im3 = axes[i,j].imshow(spectral_train_grid[i,j], cmap='inferno', norm=LogNorm(), vmin=1e-4, vmax=5)
+
+    [[axes[i, j].axis('off') for i in range(6)] for j in range(3)]
+
+    axes[0,0].set_title('800 nm')
+    axes[0,1].set_title('1150 nm')
+    axes[0,2].set_title('1500 nm')
+
+    props = dict(boxstyle='square', facecolor='k', alpha=0.5)
+    for j, text in enumerate(save_labels):
+        axes[j, 0].text(0.05, 0.075, text, transform=axes[j, 0].transAxes, fontweight='bold',
+                         color='w', fontsize=7.5, bbox=props)
+
+    cax1 = fig.add_axes([0.86, 0.645, 0.02, 0.29])
+    cax2 = fig.add_axes([0.86, 0.325, 0.02, 0.29])
+    cax3 = fig.add_axes([0.86, 0.0145, 0.02, 0.29])
+    cbar1 = plt.colorbar(im1, cax1)
+    cbar2 = plt.colorbar(im2, cax2)
+    cbar3 = plt.colorbar(im3, cax3)
+
+    cbar1.ax.tick_params(labelsize=8)
+    cbar2.ax.tick_params(labelsize=9)
+    cbar3.ax.tick_params(labelsize=10)
+
+    # plt.tight_layout()
+    plt.subplots_adjust(left=0.0, bottom=0.01, right=0.84, top=0.935, wspace=0.1, hspace=0.15)
+
+    plt.savefig('/Users/dodkins/MEDIS2train.pdf', dpi=1000)
+    plt.show(block=True)
