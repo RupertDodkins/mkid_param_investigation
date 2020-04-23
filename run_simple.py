1d65f9f9a5de7d85a150d9e447002c4f646f26e3
rdodkins <rupertdodkins@gmail.com>
2020-04-23 13:23:32 -0700

    Folder maintainence

---
+++ run_simple.py
@@ -0,0 +1,15 @@
+import medis.medis_main as mm
+from master2 import params
+params['sp'].save_to_disk = True
+params['tp'].prescription = 'general_telescope'
+params['sp'].numframes = 1
+params['ap'].n_wvl_init = 3
+params['ap'].n_wvl_final = 3
+params['sp'].memory_limit = 9.e9 /15
+params['tp'].use_atmos = True
+
+if __name__ == '__main__':
+
+    sim = mm.RunMedis(params=params, name='example1', product='photons')
+    observation = sim()
+    print(observation.keys())
