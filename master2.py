4bb4c229c8ff26f348d97c9ff3758b68dc70c76d
rdodkins <rupertdodkins@gmail.com>
2020-05-01 10:02:09 -0700

    Removing redundant and commented parameters. Updating others to be inline with the first paper draft versions

--- master2.py
+++ master2.py
@@ -14,17 +14,14 @@ params['sp'].save_to_disk = True
 
 params['ap'].companion = True
 params['ap'].star_flux = int(1e9)
-# # params['ap'].contrast = 10**np.array([-3.5, -4, -4.5, -5] * 2)
-# # params['ap'].companion_xy = [[2.5,0], [0,3], [-3.5,0], [0,-4], [4.5,0], [0,5], [-5.5,0],[0,-6]]
-# params['ap'].n_wvl_init = 8
-# params['ap'].n_wvl_final = 16
-# params['ap'].contrast = [10**-3.5]
-# params['ap'].companion_xy = [[2.5,0]]
-# # params['ap'].n_wvl_init = 3
-# # params['ap'].n_wvl_final = 3
+params['ap'].contrast = 10**np.array([-3.5, -4, -4.5, -5] * 2)
+params['ap'].companion_xy = [[2.5,0], [0,3], [-3.5,0], [0,-4], [4.5,0], [0,5], [-5.5,0],[0,-6]]
+params['ap'].n_wvl_init = 8
+params['ap'].n_wvl_final = 16
+params['sp'].numframes = 10
 
 params['tp'].prescription = 'general_telescope'
-params['sp'].beam_ratio = 0.3 #0.25
+params['sp'].beam_ratio = 0.25 # 0.3
 params['tp'].entrance_d = 8.
 params['tp'].obscure = True
 params['tp'].use_ao = True
@@ -34,35 +31,29 @@ params['tp'].platescale = 10  # mas
 params['tp'].detector = 'ideal'
 params['tp'].use_atmos = True
 params['tp'].use_zern_ab = False
-params['tp'].occulter_type = 'Vortex'
 params['tp'].aber_params = {'Phase': True, 'Amp': False}
-params['tp'].aber_vals = {'a': [5e-18, 1e-19],
-                       'b': [2.0, 0.2],
-                       'c': [3.1, 0.5]}
 params['tp'].piston_error = False
 params['ap'].wvl_range = np.array([800, 1500]) /1e9
 params['tp'].rot_rate = 0  # deg/s
 params['tp'].pix_shift = [[0, 0]]
-params['tp'].satelite_speck = False
+params['tp'].satelite_speck = True
 params['tp'].legs_frac = 0.03
 params['tp'].f_lens = 200.0 * params['tp'].entrance_d
 params['tp'].use_coronagraph = True
 params['tp'].occult_loc = [0,0]
-params['tp'].cg_type = 'Gaussian'
+params['tp'].cg_type = 'Solid'
 params['tp'].cg_size = 3  # physical size or lambda/D size
 params['tp'].cg_size_units = "l/D"  # "m" or "l/D"
 params['tp'].lyot_size = 0.75  # units are in fraction of surface blocked
 params['tp'].fl_cg_lens = params['tp'].f_lens  # m
 
-# for param in [params['sp']]:
-#     pprint(param.__dict__)
 params['mp'].phase_uncertainty = True
 params['mp'].phase_background = False
 params['mp'].QE_var = True
 params['mp'].bad_pix = True
 params['mp'].dark_counts = True
 params['mp'].dark_pix_frac = 0.1
-params['mp'].dark_bright = 20
+params['mp'].dark_bright = 2 * 1e2
 params['mp'].hot_pix = None
 params['mp'].hot_bright = 2.5 * 10 ** 3
 params['mp'].R_mean = 8
@@ -79,16 +70,5 @@ params['mp'].remove_close = False
 params['mp'].quantize_FCs = False #True
 params['mp'].wavecal_coeffs = [1.e9 / 6, -250]
 
-
-# params['ap'].contrast = 10**np.array([-3.5, -4, -4.5, -5] * 2)
-# params['ap'].companion_xy = [[2.5,0], [0,3], [-3.5,0], [0,-4], [4.5,0], [0,5], [-5.5,0],[0,-6]]
-params['ap'].contrast = [10**-3.5]
-params['ap'].companion_xy = [[2.5,0]]
-# params['ap'].n_wvl_init = 8
-# params['ap'].n_wvl_final = 16
-params['ap'].n_wvl_init = 2
-params['ap'].n_wvl_final = 2
-# params['sp'].numframes = 10
-params['sp'].numframes = 1
 
 
